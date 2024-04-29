d862b61050c56581372a2c3a0e5595467623222c
Pei-Yu-Fish <150638526+Pei-Yu-Fish@users.noreply.github.com>
2024-04-29 11:33:36 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var radioElement
 var captureGraphics
 var capture_w=640
 var capture_h=480
-var span=10
+var span=5
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -17,13 +17,15 @@ function setup() {
   radioElement.position(width/2-300,2)
   radioElement.option("方塊")
   radioElement.option("圓圈")
-  radioElement.style("color","#960018")
-  // radioElement.style("")
-
+  radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.style("color","#ffff")
+//---------------------------
+   
 }
 
 function draw() {
-  background(220);
+  background(0);
   noStroke()
   push()
     translate(width/2-capture_w/2,height/2-capture_h/2)
@@ -32,12 +34,25 @@ function draw() {
       for(var y=0;y<captureGraphics.height;y=y+span){
         var pixel=captureGraphics.get(x,y)
         fill(pixel)
-        // if(removeElements.value()=="方塊"||radioElement.value()==""){
-        //     rect(x,y,span)
-        // }
-        // if(removeElements.value()=="圓圈"){
+        if(removeElement.value()=="方塊"||radioElement.value()==""){
+            rect(x,y,span)
+        }
+        if(removeElement.value()=="圓圈"){
             ellipse(x,y,span)
-        // }
+        }
+        if(radioElement.value()=="亮度"){
+          bk=(pixel[0]+pixel[1]+pixel[2]/3)
+          fill(bk)
+          ellipse(x,y,span*map(bk,0,255,0,1))
+        }
+        if(radioElement.value()=="紅底"){
+          colorMode(HSB)
+          fill(pixel[0],80,80)
+          rectMode(CENTER)
+          rect(x,y,span)
+          fill(0)
+          ellipse(x,y,5)
+        }
       }
     }
   pop()
